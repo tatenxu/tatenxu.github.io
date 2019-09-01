32a054758008bf5c5592c4b138fbfba9c41c6ada
titanxxxxx <titanxuhaha@github.com>
2019-09-01 15:03:16 +0800

    back to index

--- assets/showdown/src/options.js
+++ assets/showdown/src/options.js
@@ -72,7 +72,7 @@ function getDefaultOpts (simple) {
       type: 'boolean'
     },
     tables: {
-      defaultValue: false,
+      defaultValue: true,
       describe: 'Turn on/off tables support',
       type: 'boolean'
     },
